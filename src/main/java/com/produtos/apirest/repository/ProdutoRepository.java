7895d3e5df2a8d6317521781bf3cf4be3bd32051
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-04-26 16:50:32 -0300

    Produto Resource

--- src/main/java/com/produtos/apirest/repository/ProdutoRepository.java
+++ src/main/java/com/produtos/apirest/repository/ProdutoRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.produtos.apirest.models.Produto;
 
-public interface PrudutoRepository extends JpaRepository<Produto, Long> {
-
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	Produto findById(long id); 
 }
