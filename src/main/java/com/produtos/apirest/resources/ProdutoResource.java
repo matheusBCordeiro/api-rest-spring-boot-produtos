d1ce4eae3220d13830ebb65b5e4f43258a9d3784
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-04-26 17:21:26 -0300

    Swagger config

--- src/main/java/com/produtos/apirest/resources/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -3,6 +3,7 @@ package com.produtos.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,33 +16,43 @@ import org.springframework.web.bind.annotation.RestController;
 import com.produtos.apirest.models.Produto;
 import com.produtos.apirest.repository.ProdutoRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API REST Produtos")
 public class ProdutoResource {
 	
 	@Autowired
 	ProdutoRepository produtoRepository;
 	
+	@ApiOperation(value="Retorna uma lista de Produtos")
 	@GetMapping("/produtos")
 	public List<Produto> listarProdutos(){
 		return produtoRepository.findAll();
 	}
 	
+	@ApiOperation(value="Retorna um único produto ")
 	@GetMapping("/produto/{id}")
 	public Produto listarProdutoPorId(@PathVariable(value="id") long id){
 		return produtoRepository.findById(id);
 	}
 	
+	@ApiOperation(value="Salva um produto")
 	@PostMapping("/produto")
 	public Produto salvarProduto(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
 	}
-	
+
+	@ApiOperation(value="Deleta um produto")
 	@DeleteMapping("/produto")
 	public void deletarProduto(@RequestBody Produto produto) {
 		 produtoRepository.delete(produto);
 	}
 	
+	@ApiOperation(value="Atualiza um produto")
 	@PutMapping("produto")
 	public Produto atualizarProduto(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
